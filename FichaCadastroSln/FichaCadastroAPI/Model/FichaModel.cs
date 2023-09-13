2b7e26ff99e60b354f0d5f4d4543e14624a49e5a
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-09-13 15:29:35 -0300

    create dataanotaions

--- FichaCadastroSln/FichaCadastroAPI/Model/FichaModel.cs
+++ FichaCadastroSln/FichaCadastroAPI/Model/FichaModel.cs
@@ -1,13 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace FichaCadastroAPI.Model
 {
     [Table("Ficha")]
     public class FichaModel
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Column(TypeName = "VARCHAR"), StringLength(250), Required]
         public string Name { get; set; }
+        [Column(TypeName = "VARCHAR"), StringLength(100), Required]
         public string Email { get; set; }
+
         public DateTime DataNascimento { get; set; }
 
     }
