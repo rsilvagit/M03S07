6a3372a417a90cd182f0b0924737b4c33a502af6
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-09-13 15:18:48 -0300

    criando propriedades

---
+++ FichaCadastroSln/FichaCadastroAPI/Model/FichaModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FichaCadastroAPI.Model
+{
+    [Table("Ficha")]
+    public class FichaModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
