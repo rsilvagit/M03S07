a7aa2a4a5ee7aeedbb9b0f769afa36a7899981b1
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-09-13 15:08:35 -0300

    create ficha model

--- FichaCadastroSln/FichaCadastroAPI/Model/FichaModel.cs
+++ FichaCadastroSln/FichaCadastroAPI/Model/FichaModel.cs
@@ -4,19 +4,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FichaCadastroAPI.Model
 {
+    [Table("Ficha")]
     public class FichaModel
     {
-        [Table("Ficha")]
-        public class FichaModel
-        {
-            [Key]
-            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-            public int Id { get; set; }
-            [Column(TypeName = "VARCHAR"), StringLength(250), Required]
-            public string Nome { get; set; }
-            [Column(TypeName = "VARCHAR"), StringLength(100), Required]
-            public string Email { get; set; }
-            public DateTime DataNascimento { get; set; }
-        }
+        public int
+        
     }
 }
