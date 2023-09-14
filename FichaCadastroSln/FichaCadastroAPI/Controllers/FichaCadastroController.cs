91de2edb5f94b5fad4c65d0895e88de86ad60da5
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-09-13 21:19:19 -0300

    iniciando controllers

---
+++ FichaCadastroSln/FichaCadastroAPI/Controllers/FichaCadastroController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using FichaCadastroAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FichaCadastroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FichaCadastroController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly FichaCadastroContextDB _db;
+
+        public FichaCadastroController(IMapper mapper, FichaCadastroContextDB db)
+        {
+            _mapper = mapper;
+            _db = db;
+        }
+        [HttpPost]
+        public ActionResult Post() {
+            return Ok();
+        }
+    }
+}
